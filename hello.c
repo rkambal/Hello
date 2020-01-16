5b5b25fdc08706d9e94e7ccddc9adbf3343286ee
rkambal <59982115+rkambal@users.noreply.github.com>
2020-01-16 17:11:25 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,6 +5,6 @@ Date: 2020/01/14
 #include <stdio.h>
 int main(void) {
 	printf("Hello World!\n");
-	printf("\n My Major is Computer Science (and Engineering)
+	printf("\n My Major is Computer Science (and Engineering)");
 	return 0;
 }
